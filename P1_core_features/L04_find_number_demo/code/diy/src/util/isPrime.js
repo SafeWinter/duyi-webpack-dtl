f621c76278d8187db24193a71d42ef5522790c3c
SafeWinter <zandong_19@aliyun.com>
2026-02-04 03:56:39 +0800

    optimized isPrime: focus on odd numbers only

--- P1_core_features/L04_find_number_demo/code/diy/src/util/isPrime.js
+++ P1_core_features/L04_find_number_demo/code/diy/src/util/isPrime.js
@@ -1,12 +1,12 @@
 export default (n) => {
-  if(n < 2) {
+  if(n < 2 || n % 2 === 0) {
     return false;
   }
   
-  for(let i = 2, len = Math.ceil(n ** 0.5); i <= len; i++) {
+  for(let i = 3, len = Math.sqrt(n); i <= len; i+=2) {
     if(n % i === 0) {
       return false;
     }
   }
   return true;
-};
+}
