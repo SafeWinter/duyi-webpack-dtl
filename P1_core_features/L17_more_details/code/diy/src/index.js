24cd67a13e07615b7585a67932f9dd3c43f6c65a
SafeWinter <zandong_19@aliyun.com>
2026-02-07 19:10:28 +0800

    DIY: try stats option

--- P1_core_features/L17_more_details/code/diy/src/index.js
+++ P1_core_features/L17_more_details/code/diy/src/index.js
@@ -1,12 +1,1 @@
-const $ = require('jquery');
-const _ = require('lodash');
-
-$(function() {
-  $('h1').css({
-    'font-style': 'italic',
-    'background': '#eee'
-  });
-});
-
-const arr = _.map([1, 2, 3], n => n ** 2);
-console.log('arr:', arr);
+console.log('module index');
--- P1_core_features/L17_more_details/code/diy/webpack.config.js
+++ P1_core_features/L17_more_details/code/diy/webpack.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
-  externals: {
-    jquery: '$',
-    lodash: '_'
+  stats: {
+    colors: true, // 彩色输出
+    modules: false,  // 不展示模块明细
+    hash: false, // 不显示总 Hash 值
+    builtAt: false  // 不显示打包时间
   }
 };
