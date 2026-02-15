6cfbc3dd7b38adaa7187f5b62ca9732c6ea1257d
SafeWinter <zandong_19@aliyun.com>
2026-02-15 18:13:39 +0800

    DIY: test "useBuiltIns": "usage" in @babel/preset-env

--- P4_JS_compatibilities/L37_babel_presets/code/diy/dist/index.js
+++ P4_JS_compatibilities/L37_babel_presets/code/diy/dist/index.js
@@ -1,12 +1,15 @@
 "use strict";
 
-var a = 1;
-var b = function b() {
-  return console.log('b');
-};
-var Method = function Method() {
-  var _this = this;
-  var c = function c() {
-    console.log(_this);
-  };
-};
+require("core-js/modules/es.object.to-string.js");
+require("core-js/modules/es.promise.js");
+// const a = 1;
+
+// const b = () => console.log('b');
+
+// const Method = function() {
+//   const c = () => {
+//     console.log(this);
+//   }
+// }
+
+var p = Promise.resolve(123).then(console.log).catch(console.error);
--- P4_JS_compatibilities/L37_babel_presets/code/diy/src/index.js
+++ P4_JS_compatibilities/L37_babel_presets/code/diy/src/index.js
@@ -1,9 +1,13 @@
-const a = 1;
+// const a = 1;
 
-const b = () => console.log('b');
+// const b = () => console.log('b');
 
-const Method = function() {
-  const c = () => {
-    console.log(this);
-  }
-}
+// const Method = function() {
+//   const c = () => {
+//     console.log(this);
+//   }
+// }
+
+const p = Promise.resolve(123)
+  .then(console.log)
+  .catch(console.error);
