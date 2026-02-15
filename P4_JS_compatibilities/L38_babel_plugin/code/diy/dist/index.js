4e66db3b48d011bd2ab4c3866a3962456c4eb5e9
SafeWinter <zandong_19@aliyun.com>
2026-02-15 22:36:43 +0800

    added only source code for 17_Webpack:L38: Babel plugins

---
+++ P4_JS_compatibilities/L38_babel_plugin/code/diy/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function Print() {}
+var obj = {
+  loginId: "abc"
+};
+Print.call(obj); // 相当于 Print.call(obj);
---
+++ P4_JS_compatibilities/L38_babel_plugin/code/diy/src/index.js
@@ -0,0 +1,13 @@
+function Print() {
+    console.log(this.loginId);
+}
+
+const obj = {
+    loginId: "abc"
+};
+
+obj::Print(); // 相当于 Print.call(obj);
+
+
+console.log("foo");
+console.error("bar");
