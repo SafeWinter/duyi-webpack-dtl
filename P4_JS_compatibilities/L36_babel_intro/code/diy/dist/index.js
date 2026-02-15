8af649c3a9daf6e77a5d50a3a7bf1a70f9575354
SafeWinter <zandong_19@aliyun.com>
2026-02-15 17:13:41 +0800

    added notes for 17_Webpack:L36: Babel introduction

---
+++ P4_JS_compatibilities/L36_babel_intro/code/diy/dist/index.js
@@ -0,0 +1,3 @@
+const map = new Map([['aaa', 'AAA'], ['bbb', 'BBB'], ['ccc', 'CCC']]);
+console.log(map.get('ccc'));
+console.log(map.size);
---
+++ P4_JS_compatibilities/L36_babel_intro/code/diy/src/index.js
@@ -0,0 +1,8 @@
+const map = new Map([
+  ['aaa', 'AAA'],
+  ['bbb', 'BBB'],
+  ['ccc', 'CCC']
+]);
+
+console.log(map.get('ccc'));
+console.log(map.size);
