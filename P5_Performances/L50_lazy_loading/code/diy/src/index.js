dfaffee752db87b736450bdc70602ddb0cd3d61e
SafeWinter <zandong_19@aliyun.com>
2026-02-20 00:48:31 +0800

    DIY: rename the lazy loaded chunk with /* webpackChunkName: "lodash" */

--- P5_Performances/L50_lazy_loading/code/diy/src/index.js
+++ P5_Performances/L50_lazy_loading/code/diy/src/index.js
@@ -1,11 +1,7 @@
-const getChunk = () => import("./util");
-
 console.log("module index");
 
 const btn = document.querySelector("button");
 btn.addEventListener("click", async (ev) => {
-  const { chunk } = await getChunk(); 
-  // OR:
-  // const { chunk } = await import('./util');
+  const { chunk } = await import(/* webpackChunkName: "lodash" */'./util'); 
   console.log(chunk([1, 2, 3, 4, 5, 6], 2));
 });
