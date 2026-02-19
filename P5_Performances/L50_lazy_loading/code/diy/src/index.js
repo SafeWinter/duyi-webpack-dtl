ba2e490f0697f9930bc78d0792de822972c154c8
SafeWinter <zandong_19@aliyun.com>
2026-02-20 00:19:23 +0800

    DIY: without lazy loading (much smaller)

--- P5_Performances/L50_lazy_loading/code/diy/src/index.js
+++ P5_Performances/L50_lazy_loading/code/diy/src/index.js
@@ -1,7 +1,8 @@
+import {chunk} from 'lodash-es';
+
 console.log("module index");
 
 const btn = document.querySelector("button");
-btn.addEventListener("click", async (ev) => {
-  const { chunk } = await import("lodash-es");
+btn.addEventListener("click", (ev) => {
   console.log(chunk([1, 2, 3, 4, 5, 6], 2));
 });
