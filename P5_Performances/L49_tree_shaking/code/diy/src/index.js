f80d4adc57308494a5421e1cdb4a8758d047b28d
SafeWinter <zandong_19@aliyun.com>
2026-02-19 23:41:20 +0800

    DIY: try CSS Tree Shaking via purgecss & glob-all plugins

--- P5_Performances/L49_tree_shaking/code/diy/src/index.js
+++ P5_Performances/L49_tree_shaking/code/diy/src/index.js
@@ -1,3 +1,6 @@
-import "./common.js";
+import "./styles.css";
 
 console.log('module index');
+
+const header = document.querySelector('.heading');
+header.classList.add('violet');
--- P5_Performances/L49_tree_shaking/code/diy/webpack.config.js
+++ P5_Performances/L49_tree_shaking/code/diy/webpack.config.js
@@ -1,5 +1,13 @@
 const { cleanWebpackPlugin, htmlWebpackPlugin } = require('./plugins');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const PurgePlugin = require('purgecss-webpack-plugin');
+
+const path = require('node:path');
+const src = path.resolve(__dirname, 'src');
+const htmlPath = path.resolve(__dirname, 'public/index.html');
+const { sync } = require('glob-all');
+const paths = sync([`${src}**/*.js`, htmlPath]);
+console.log(paths);
 
 module.exports = {
   mode: 'development',
@@ -12,6 +20,9 @@ module.exports = {
     htmlWebpackPlugin,
     new MiniCssExtractPlugin({
       filename: '[name].[hash:5].css'
+    }),
+    new PurgePlugin({
+      paths
     })
   ],
   stats: {
