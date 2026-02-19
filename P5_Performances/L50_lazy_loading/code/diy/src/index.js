d175735d5cfade320762081ec9bed10cd405edd3
SafeWinter <zandong_19@aliyun.com>
2026-02-20 00:37:04 +0800

    DIY: apply Tree Shaking to lazy import

--- P5_Performances/L50_lazy_loading/code/diy/src/index.js
+++ P5_Performances/L50_lazy_loading/code/diy/src/index.js
@@ -1,8 +1,11 @@
-import {chunk} from 'lodash-es';
+const getChunk = () => import("./util");
 
 console.log("module index");
 
 const btn = document.querySelector("button");
-btn.addEventListener("click", (ev) => {
+btn.addEventListener("click", async (ev) => {
+  const { chunk } = await getChunk(); 
+  // OR:
+  // const { chunk } = await import('./util');
   console.log(chunk([1, 2, 3, 4, 5, 6], 2));
 });
