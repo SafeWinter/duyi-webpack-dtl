79051f586cf67eaca62958720f22db89e857dba4
SafeWinter <zandong_19@aliyun.com>
2026-02-20 16:40:14 +0800

    DIY: try lazy load with lodash-es/chunk

--- P5_Performances/L52_bundle_analyzer/code/diy/src/index.js
+++ P5_Performances/L52_bundle_analyzer/code/diy/src/index.js
@@ -1,10 +1,10 @@
 import $ from "jquery";
-import { chunk } from "lodash-es";
 
 $(function () {
   $(".heading")
     .css("font-style", "italic")
     .on("click", async function (ev) {
+      const {default: chunk} = await import('lodash-es/chunk');
       console.log(chunk([1, 2, 3, 4, 5, 6], 2));
     });
 });
