18728cec9fab5c0463dbd7cd4d3af873e76e0b5f
SafeWinter <zandong_19@aliyun.com>
2026-02-20 12:42:20 +0800

    DIY: cancel the enforced code style

--- P5_Performances/L51_ESLint/code/diy/src/index.js
+++ P5_Performances/L51_ESLint/code/diy/src/index.js
@@ -1,11 +1,4 @@
-console.log('abc');
-
-const a = 1,
-  b = 2,
-  c = a + b;
-
-if(c > 2 * Math.random() + 2) {
-  console.log('1');
-} else {
-  console.log('2');
+let a = 1;
+function abc() {
+   return 0;
 }
